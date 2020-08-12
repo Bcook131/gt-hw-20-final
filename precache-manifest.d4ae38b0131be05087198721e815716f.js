cf03e51ece2649a7d77bcc0d6f93257aa464bd74
Brian Cook <bcrazorrider@gmail.com>
2020-08-12 15:51:04 -0400

    Updates

---
+++ precache-manifest.d4ae38b0131be05087198721e815716f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c263208c5815cbef058508cd046908",
+    "url": "/gt-hw-20-final/index.html"
+  },
+  {
+    "revision": "2b084d1aca0979b3e2eb",
+    "url": "/gt-hw-20-final/static/css/main.b6c5ba28.chunk.css"
+  },
+  {
+    "revision": "bef8791640ff757bb20b",
+    "url": "/gt-hw-20-final/static/js/2.e6ebb2a9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gt-hw-20-final/static/js/2.e6ebb2a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b084d1aca0979b3e2eb",
+    "url": "/gt-hw-20-final/static/js/main.051ebf31.chunk.js"
+  },
+  {
+    "revision": "0eb7bdc2084c94915da7",
+    "url": "/gt-hw-20-final/static/js/runtime-main.744c7826.js"
+  }
+]);
